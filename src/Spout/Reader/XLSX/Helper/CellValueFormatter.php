e9e5f5c8e907792bdada1e53015d04aa8e5ffdda
Marcus Vinicius Ventura Bortolotti <marcus.bortolotti@slingshotdigital.com.br>
2019-07-05 16:49:33 -0300

    Fix formatInlineStringCellValue with no <t> node

--- src/Spout/Reader/XLSX/Helper/CellValueFormatter.php
+++ src/Spout/Reader/XLSX/Helper/CellValueFormatter.php
@@ -133,7 +133,7 @@ class CellValueFormatter
         // inline strings are formatted this way:
         // <c r="A1" t="inlineStr"><is><t>[INLINE_STRING]</t></is></c>
         $tNode = $node->getElementsByTagName(self::XML_NODE_INLINE_STRING_VALUE)->item(0);
-        $cellValue = $this->escaper->unescape($tNode->nodeValue);
+        $cellValue = $this->escaper->unescape(!empty($tNode) ? $tNode->nodeValue : '');
 
         return $cellValue;
     }
